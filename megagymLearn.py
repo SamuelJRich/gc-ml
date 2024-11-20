bbc38467ba920750b794e0049e74e80deb95dfb1
SamuelJRich <120282121+SamuelJRich@users.noreply.github.com>
2024-11-20 20:07:46 +0000

    feat: Imported required scikit-learn libraries/modules.

--- megagymLearn.py
+++ megagymLearn.py
@@ -1,6 +1,8 @@
 ## This is for muscle group based exercise training, as well as intensity/training level. ##
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
 ## This is for muscle group based exercise training, as well as intensity/training level.
 
