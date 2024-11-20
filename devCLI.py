e8a3b78e48444a02addaf670b569581708b54c39
SamuelJRich <120282121+SamuelJRich@users.noreply.github.com>
2024-11-20 14:34:09 +0000

    feat: Lists to be referenced when comparing user input to avail choices.

--- devCLI.py
+++ devCLI.py
@@ -1,2 +1,10 @@
 # Used for 'simulating' user-data collection during the development phase.
 
+goals = ['Strength', 'Size', 'Cardio']
+
+targets = ['Chest(Pectorals)', 'Back(Latissimus Dorsi)', 'Back(Traps)', 'Back(Spinal Erectors)',
+           'Biceps', 'Triceps', 'Shoulders(Deltoid)', 'Abdominal', 'Legs(Hamstrings)', 'Legs(Quadriceps)', 'Legs(Glutes)']
+
+equipment = ['Bodyweight', 'Resistance band', 'Dumbbells', 'Machines', 'Kettlebell', 'Barbell']
+
+level = ['Beginner', 'Intermediate', 'Advanced']
