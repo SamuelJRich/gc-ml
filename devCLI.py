b0a6739a770eebe1f1f54e5dce7d2bc384d1ae46
SamuelJRich <120282121+SamuelJRich@users.noreply.github.com>
2024-11-20 14:36:14 +0000

    feat: Created a function that collects CLI input.

--- devCLI.py
+++ devCLI.py
@@ -8,3 +8,35 @@ targets = ['Chest(Pectorals)', 'Back(Latissimus Dorsi)', 'Back(Traps)', 'Back(Sp
 equipment = ['Bodyweight', 'Resistance band', 'Dumbbells', 'Machines', 'Kettlebell', 'Barbell']
 
 level = ['Beginner', 'Intermediate', 'Advanced']
+
+
+def collect():
+    
+    userGoals = []
+    userTargets = []
+    userEquipment = []
+    userLevel = []
+    inputsCollected = 4
+    
+    while inputsCollected != 0:
+        
+        print('Strength, Size, Cardio')
+        goalsIn = input('Enter your current fitness goal: ')
+        inputsCollected -=1
+        print('Chest, Back, Biceps, Triceps, Shoulders, Abdominal, Legs')
+        targetsIn = input('Enter the areas you would like to target: ')
+        inputsCollected -=1
+        print('Bodyweight, Resistance band, Dumbbells, Machines, Kettlebell, Barbell')
+        equipmentIn = input('Enter your current equipment: ')
+        inputsCollected -=1
+        print('Beginner, Intermediate, Advanced')
+        levelIn = input('Enter your current level: ')
+        inputsCollected -=1
+        
+    print('~ ~ All data collected ~ ~') 
+
+    
+collect()
+        
+    
+    
