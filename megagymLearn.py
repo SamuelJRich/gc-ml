582aef9e81101ad08275b25e1b3f167dfd5f0922
SamuelJRich <120282121+SamuelJRich@users.noreply.github.com>
2024-11-20 20:16:02 +0000

    feat: Dropping the irrelevant feature columns, as well as the blank or Null value

--- megagymLearn.py
+++ megagymLearn.py
@@ -8,6 +8,9 @@ from sklearn.preprocessing import LabelEncoder
 # df is dataframe
 df = pd.read_csv("megaGymDataset.csv")
 
+# .drop removes the respective columns, .dropna removes any rows that contain NaN within the data.
+df = df.drop(columns=['Desc', 'Rating', 'RatingDesc'])
+df = df.dropna()
 
 #print("All types of:\n", df['Level'].unique())
 print("50 rows:\n", df[2280:2290])
