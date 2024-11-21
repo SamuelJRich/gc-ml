12931cb069defe113bc47d0f2102cd7f6732b41a
SamuelJRich <120282121+SamuelJRich@users.noreply.github.com>
2024-11-21 14:37:46 +0000

    feat: Included relevant imports.

--- d1.py
+++ d1.py
@@ -1,1 +1,5 @@
 ## This is for muscle group based exercise training, as well as intensity/training level. ##
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, OrdinalEncoder
