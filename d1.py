28230f948aa8a305beeb2f1371e517e1f80985f9
SamuelJRich <120282121+SamuelJRich@users.noreply.github.com>
2024-11-21 14:38:16 +0000

    feat: Read data, drop NaNs, and irrelevant columns.

--- d1.py
+++ d1.py
@@ -3,3 +3,10 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder, OrdinalEncoder
+
+
+# df is dataframe
+df = pd.read_csv("d1.csv")
+
+# .drop removes the respective columns, .dropna removes any rows that contain NaN within the data.
+df = df.drop(columns=['Desc', 'Rating', 'RatingDesc'])
