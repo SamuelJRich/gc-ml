8c6b6fd8168ce126f5e4a0d2d1f6777202395a46
SamuelJRich <120282121+SamuelJRich@users.noreply.github.com>
2024-11-25 17:55:59 +0000

    refactor: Removed unused imports.

--- d1.py
+++ d1.py
@@ -2,7 +2,6 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder, OrdinalEncoder
 
 
 # df is dataframe
